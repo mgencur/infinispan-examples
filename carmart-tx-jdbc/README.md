e4e00c40eb13469a946c4565db40d25eee8fee0e
Martin Gencur <mgencur@redhat.com>
2012-05-08 21:11:35 +0200

    Add Carmart app  configured with transactions and Jdbc cache store backup

---
+++ carmart-tx-jdbc/README.md
@@ -0,0 +1,99 @@
+How to run the example
+======================
+
+CarMart is a simple web application that uses Infinispan which is backed by a relational 
+database (locally installed MySQL DB).
+Users can list cars, add new cars or remove them from the CarMart. Information about each car
+is stored in a cache. The application also shows cache statistics like stores, hits, retrievals, etc.
+
+Caches are configured programatically and run in the same JVM as the web application.
+
+
+Building and deploying to JBoss AS 7
+------------------------------------
+
+0) Prepare local MySQL database:
+
+    `carmartdb`
+    
+   acessible with the following credentials:
+   
+    `username: carmart`
+    `password: carmart`
+
+1) Adjust datasources configuration in the configuration file for JBossAS ($JBOSS_HOME/standalone/configuration/standalone.xml):
+
+    `<subsystem xmlns="urn:jboss:domain:datasources:1.0">
+        <datasources>
+            <datasource jndi-name="java:jboss/datasources/ExampleDS" pool-name="ExampleDS" enabled="true" use-java-context="true">
+                <connection-url>jdbc:mysql://localhost:3306/carmartdb</connection-url>
+                <driver>mysql-connector-java-5.1.17-bin.jar</driver>
+                <security>
+                    <user-name>carmart</user-name>
+                    <password>carmart</password>
+                </security>
+            </datasource>
+        </datasources>
+    </subsystem>`
+
+2) Copy Jdbc driver jar file for MySQL DB into $JBOSS_HOME/standalone/deployments, 
+   e.g. mysql-connector-java-5.1.17-bin.jar
+
+3) Start JBoss AS 7 where your application will run
+
+    `$JBOSS_HOME/bin/standalone.sh`
+
+4) Build and deploy the application
+
+    `mvn clean package jboss-as:deploy -Pjbossas`
+
+5) Go to http://localhost:8080/carmart-tx-jdbc
+
+6) Undeploy the application
+
+    `mvn jboss-as:undeploy`
+
+
+Building and deploying to Tomcat 7
+------------------------------------
+
+0) Prepare local MySQL database:
+
+    `carmartdb`
+    
+   acessible with the following credentials:
+   
+    `username: carmart`
+    `password: carmart`
+
+1) Copy Jdbc driver jar file for MySQL DB into $TOMCAT_HOME/lib, 
+   e.g. mysql-connector-java-5.1.17-bin.jar
+
+2) Start Tomcat 7 where your application will run
+
+    `$TOMCAT_HOME/bin/catalina.sh start`
+
+3) Build and deploy the application
+
+    `mvn clean package tomcat:deploy -Ptomcat`
+
+NOTE: You need to add the following snippet to your configuration file for Maven, i.e. ~/.m2/settings.xml:
+
+    `<server>
+         <id>tomcat</id>
+         <username>admin</username>
+         <password>admin</password>
+    </server>`
+    
+    and configure Tomcat in the same way - add user admin to $TOMCAT_HOME/conf/tomcat-users.xml:
+    
+    `<role rolename="manager-script"/>
+     <user username="admin" password="admin" roles="manager-script"/>`
+     
+     After that, Maven will be able to deploy the application to running Tomcat container.
+
+4) Go to http://localhost:8080/carmart-tx-jdbc
+
+5) Undeploy the application
+
+    `mvn jboss-as:undeploy`
---
+++ carmart-tx-jdbc/src/tomcat/java/org/infinispan/examples/carmart/session/TomcatCacheContainerProvider.java
@@ -0,0 +1,98 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.infinispan.examples.carmart.session;
+
+import java.util.logging.Logger;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import org.infinispan.api.BasicCacheContainer;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.eviction.EvictionStrategy;
+import org.infinispan.examples.carmart.session.CacheContainerProvider;
+import org.infinispan.loaders.jdbc.stringbased.JdbcStringBasedCacheStore;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
+import org.infinispan.transaction.lookup.JBossStandaloneJTAManagerLookup;
+import org.infinispan.util.concurrent.IsolationLevel;
+
+/**
+ * {@link CacheContainerProvider}'s implementation creating a DefaultCacheManager 
+ * which is configured programmatically. Infinispan's libraries need to be bundled 
+ * with the application - this is called "library" mode.
+ * 
+ * The only difference from JBossASCacheContainerProvider is the transaction 
+ * manager lookup class.
+ * 
+ * @author Martin Gencur
+ * 
+ */
+@ApplicationScoped
+public class TomcatCacheContainerProvider implements CacheContainerProvider {
+
+    private BasicCacheContainer manager;
+
+    public BasicCacheContainer getCacheContainer() {
+        if (manager == null) {
+            GlobalConfiguration glob = new GlobalConfigurationBuilder()
+            .nonClusteredDefault().globalJmxStatistics().enable()
+            .jmxDomain("org.infinispan.carmart.tx-jdbc")
+            .build();
+        Configuration loc = new ConfigurationBuilder()
+            .clustering().cacheMode(CacheMode.LOCAL)
+            .transaction().transactionMode(TransactionMode.TRANSACTIONAL).autoCommit(false)
+            .lockingMode(LockingMode.OPTIMISTIC).transactionManagerLookup(new JBossStandaloneJTAManagerLookup())
+            .locking().isolationLevel(IsolationLevel.REPEATABLE_READ)
+            .loaders().passivation(false).preload(false).shared(false)
+            .addCacheLoader().cacheLoader(new JdbcStringBasedCacheStore()).fetchPersistentState(false).purgeOnStartup(true)
+            .addProperty("stringsTableNamePrefix", "carmart_table")
+            .addProperty("idColumnName", "ID_COLUMN")
+            .addProperty("dataColumnName", "DATA_COLUMN")
+            .addProperty("timestampColumnName", "TIMESTAMP_COLUMN")
+            .addProperty("timestampColumnType", "BIGINT")
+            .addProperty("connectionFactoryClass", "org.infinispan.loaders.jdbc.connectionfactory.ManagedConnectionFactory")
+            .addProperty("connectionUrl", "jdbc:mysql://localhost:3306/carmartdb")
+            .addProperty("userName", "carmart")
+            .addProperty("driverClass", "com.mysql.jdbc.Driver")
+            .addProperty("idColumnType", "VARCHAR(255)")
+            .addProperty("dataColumnType", "VARBINARY(1000)")
+            .addProperty("dropTableOnExit", "false")
+            .addProperty("createTableOnStart", "true")
+            .addProperty("databaseType", "MYSQL")
+            .addProperty("datasourceJndiLocation", "java:comp/env/jdbc/ExampleDB")
+            .build();
+            manager = new DefaultCacheManager(glob, loc, true);
+        }
+        return manager;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        manager.stop();
+        manager = null;
+    }
+}
